73bdbab64ac429ecdc9691476828038dd97d2e42
dev-david-chen <105615428+dev-david-chen@users.noreply.github.com>
2022-05-23 10:48:55 +0800

    Add files via upload

---
+++ Chap05_DataAccesswithSpringBoot/Embedded Redis/src/main/java/com/apress/prospringboot2/repository/ToDoRepository.java
@@ -0,0 +1,10 @@
+package com.apress.prospringboot2.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.apress.prospringboot2.domain.ToDo;
+
+
+public interface ToDoRepository extends CrudRepository<ToDo,String> {
+
+}
