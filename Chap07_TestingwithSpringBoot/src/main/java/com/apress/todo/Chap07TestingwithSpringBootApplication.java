f4905ad18d5d161237d2ce1b8b75215576e56379
dev-david-chen <105615428+dev-david-chen@users.noreply.github.com>
2022-05-24 15:02:49 +0800

    Add files via upload

---
+++ Chap07_TestingwithSpringBoot/src/main/java/com/apress/todo/Chap07TestingwithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.apress.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap07TestingwithSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap07TestingwithSpringBootApplication.class, args);
+	}
+
+}
